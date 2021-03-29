a7000d1908af9c036eafed8911256a41994561ef
jBreznar <kiwi.breznar@gmail.com>
2021-03-29 23:59:44 +0200

    Zemljevid prikazan cez celotni ekran, zoom to lokacija.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 import * as Leaflet from 'leaflet';
 
 @Component({
@@ -9,21 +10,27 @@ import * as Leaflet from 'leaflet';
 export class Tab1Page implements OnInit, OnDestroy {
   map: Leaflet.Map;
 
-  constructor() { }
+  constructor(private geolocation: Geolocation) { }
 
   ngOnInit() { 
     if (this.map == null) {
       this.leafletMap();
+      
+      this.geolocation.getCurrentPosition().then((resp) => {
+        this.map.flyTo(new Leaflet.LatLng(resp.coords.latitude, resp.coords.longitude), 16);
+      }).catch((error) => {
+        console.log('Error getting location', error);
+      });
     }
   }
 
   leafletMap() {
-    this.map = Leaflet.map('mapId').setView([46.119944, 15.005333], 8.85);
+    this.map = Leaflet.map('mapId').setView([46.119944, 15.005333], 7);
+    this.map.invalidateSize();
     Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
   }
 
   ngOnDestroy() {
     this.map.remove();
   }
-
 }
