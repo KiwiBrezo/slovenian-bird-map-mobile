dd0e201da80c7ab810dbd272bb98d337abe47830
jBreznar <kiwi.breznar@gmail.com>
2021-07-20 23:29:39 +0200

    Urejanje stila, pridobivanje statistika za uporabnika iz streznika.

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { from, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  userData = {};
+  userDataObservable: Observable<any>;
+  getUserDataRequest: Promise<any>; 
 
-  constructor() {}
+  constructor() {
+    var params = "userID=1";
+    this.getUserDataRequest = new Promise(resolve => {
+      var request = new XMLHttpRequest();
+      request.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          resolve(JSON.parse(this.responseText));
+        }
+      };
+      request.open("GET", "http://164.8.9.88:8080/slovenian-bird-map/api/analysis/user/getNumberOfObservations?" + params, true);
+      request.send();
+    });
+  }
 
+  ngOnInit() {
+    this.userDataObservable = from(this.getUserDataRequest);
+    this.userDataObservable.subscribe(data => {
+      this.userData = data;
+    });
+  }
 }
